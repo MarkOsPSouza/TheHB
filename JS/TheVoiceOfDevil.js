d9204a31f736cf2e8f106b902655f7854dad2ba0
Marcos Paulo <139388753+MarkOsPSouza@users.noreply.github.com>
2024-08-05 23:53:33 -0300

    Update TheVoiceOfDevil.js

--- JS/TheVoiceOfDevil.js
+++ JS/TheVoiceOfDevil.js
@@ -2,7 +2,7 @@ function verificarSenha() {
     var senhaInserida = document.getElementById("codigo").value;
 
     if (senhaInserida === "i heard his voice behind the bookshelf") {
-        window.location.href = "11_TheBookShelf.html";
+        window.location.href = "12_TheBookShelf.html";
     } else {
         alert("Error.");
     }
